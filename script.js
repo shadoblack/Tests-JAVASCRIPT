72016ef49bc43ded56fb66d1aa6f116d279e8f2a
unknown <edgarerb@hotmail.es>
2023-08-03 16:29:26 +0200

    test2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var nombreDeLaVariable;
+let nombreDeLaVariable1;
+const nombreDeLaVariable2 = 'siempre definida/inicializada';
+let x, y, z;
+x = 1;
+y = 2;
+z = x + y; //ojito con la ubicacion de memoria,no recomendable
 
-
-document.getElementById('demostracion').innerHTML='texto transformado por JS'
+document.getElementById('demostracion').innerHTML = 'texto transformado por JS'
